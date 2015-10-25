c29be56780afdb6f0ce320f9c2b52a596feaa3f1
Maroun Abi ramia <marounab27@gmail.com>
2015-10-25 01:23:22 -0700

    Temporary fix for pods/GoogleMobileAds

---
+++ g21b/LocalNotificationSchedular.swift
@@ -0,0 +1,13 @@
+//
+//  LocalNotificationSchedular.swift
+//  g21b
+//
+//  Created by Maroun Abi Ramia on 10/24/15.
+//  Copyright © 2015 Maroun Abi Ramia. All rights reserved.
+//
+
+import Cocoa
+
+class LocalNotificationSchedular: NSObject {
+
+}
---
+++ g21b/g21b-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  g21b-Bridging_Header.h
+//  g21b
+//
+//  Created by Maroun Abi Ramia on 10/25/15.
+//  Copyright © 2015 Maroun Abi Ramia. All rights reserved.
+//
+
+#ifndef g21b_Bridging_Header_h
+#define g21b_Bridging_Header_h
+
+#import <GADBannerView.h>
+@import GoogleMobileAds;
+
+#endif /* g21b_Bridging_Header_h */
